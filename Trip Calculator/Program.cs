0b7f56f92e8ab6161d4a0ea292ab90f5eb772f92
Z Bradford <zoltar3114@gmail.com>
2026-09-24 18:13:03 -0400

    Start Of My Program

---
+++ Trip Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿//Part One: Road Trip
+//Questions
+Console.Write("How Many Miles For Your Trip? ");
+double tripMiles = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("How Many Miles Per Gallon Can Your Car Go? ");
+double milesPerGallon = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("How Much Did You Pay For Gas? ");
+double pricePerGallon = Convert.ToDouble(Console.ReadLine());
+//Calc
+double gallonsNeeded = tripMiles / milesPerGallon;
+double fuelCost = gallonsNeeded * pricePerGallon;
+
+//Print The Calculations
+Console.WriteLine("Gallons Needed " + gallonsNeeded.ToString("F2"));
+Console.WriteLine("Fuel Cost " + fuelCost.ToString("C"));
